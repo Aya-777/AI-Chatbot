f72cd8d153a8eacea5f56f45fe3c7f0dd32fb9e9
Aya yassien <yassienaya714@gmail.com>
2025-07-19 13:44:21 +0300

    Adding fake messages and the Controls(textarea and sendButton)

--- AI Chatbot/src/App.jsx
+++ AI Chatbot/src/App.jsx
@@ -1,6 +1,11 @@
+import { useState } from 'react';
 import styles from './App.module.css';
+import { Chat } from './components/Chat/Chat';
+import { Controls } from './components/Controls/Controls';
 
 function App() {
+  const [messages, setMessages] = useState(MESSAGES);
+
   return (
     <div className={styles.App}>
       <header className={styles.Header}>
@@ -9,9 +14,47 @@ function App() {
           AI Chatbot
         </h2>
       </header>
-      <div className={styles.ChatContainer}></div>
+      <div className={styles.ChatContainer}>
+        <Chat messages={messages} />
+      </div>
+      <Controls />
     </div>
   );
 }
+
+const MESSAGES = [
+  {
+    role: 'user',
+    content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla molestiae libero aperiam eum velit? Inventore maxime incidunt nisi cupiditate officia cum vitae recusandae impedit, suscipit dolorum ipsum dolore, aliquam blanditiis?"
+  },
+  {
+    role: 'assistant',
+    content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla molestiae libero aperiam eum velit? Inventore maxime incidunt nisi cupiditate officia cum vitae recusandae impedit, suscipit dolorum ipsum dolore, aliquam blanditiis?"
+  },
+  {
+    role: 'user',
+    content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla molestiae libero aperiam eum velit? Inventore maxime incidunt nisi cupiditate officia cum vitae recusandae impedit, suscipit dolorum ipsum dolore, aliquam blanditiis?"
+  },
+  {
+    role: 'assistant',
+    content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla molestiae libero aperiam eum velit? Inventore maxime incidunt nisi cupiditate officia cum vitae recusandae impedit, suscipit dolorum ipsum dolore, aliquam blanditiis?"
+  },
+  {
+    role: 'user',
+    content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla molestiae libero aperiam eum velit? Inventore maxime incidunt nisi cupiditate officia cum vitae recusandae impedit, suscipit dolorum ipsum dolore, aliquam blanditiis?"
+  },
+  {
+    role: 'assistant',
+    content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla molestiae libero aperiam eum velit? Inventore maxime incidunt nisi cupiditate officia cum vitae recusandae impedit, suscipit dolorum ipsum dolore, aliquam blanditiis?"
+  },
+  {
+    role: 'user',
+    content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla molestiae libero aperiam eum velit? Inventore maxime incidunt nisi cupiditate officia cum vitae recusandae impedit, suscipit dolorum ipsum dolore, aliquam blanditiis?"
+  },
+  {
+    role: 'assistant',
+    content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla molestiae libero aperiam eum velit? Inventore maxime incidunt nisi cupiditate officia cum vitae recusandae impedit, suscipit dolorum ipsum dolore, aliquam blanditiis?"
+  },
+]
 
 export default App
