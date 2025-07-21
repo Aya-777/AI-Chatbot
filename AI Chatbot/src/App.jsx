2b738e0be131629be76fa64bb36e5d97efc6c744
Aya yassien <yassienaya714@gmail.com>
2025-07-21 12:55:07 +0300

    installed the right library

--- AI Chatbot/src/App.jsx
+++ AI Chatbot/src/App.jsx
@@ -2,6 +2,19 @@ import { useState } from 'react';
 import styles from './App.module.css';
 import { Chat } from './components/Chat/Chat';
 import { Controls } from './components/Controls/Controls';
+// import { GoogleGenAI } from "@google/genai";
+
+// const apiKey = import.meta.env.VITE_GOOGLE_AI_API_KEY;
+
+// if (!apiKey) {
+//   throw new Error("API key is missing. Check your .env file and Vite config.");
+// }
+
+// const ai = new GoogleGenAI({apiKey: apiKey});
+// const gemini = ai.models.generateContent({
+//   model: "gemini-1.5-flash"
+// });
+// const chat = gemini.startChat({history: []});
 
 function App() {
   const [messages, setMessages] = useState([]);
@@ -16,7 +29,7 @@ function App() {
   return (
     <div className={styles.App}>
       <header className={styles.Header}>
-        <img className={styles.Logo} src="./public/chat-bot.png" />
+        <img className={styles.Logo} src="/chat-bot.png" />
         <h2 className={styles.Title}>
           AI Chatbot
         </h2>
