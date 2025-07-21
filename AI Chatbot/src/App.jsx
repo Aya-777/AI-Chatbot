4ad539b36fc384b51132defe439dc40dc4d7970d
Aya yassien <yassienaya714@gmail.com>
2025-07-21 13:05:50 +0300

    downloading the right google lib and accessing gemini ai

--- AI Chatbot/src/App.jsx
+++ AI Chatbot/src/App.jsx
@@ -2,28 +2,29 @@ import { useState } from 'react';
 import styles from './App.module.css';
 import { Chat } from './components/Chat/Chat';
 import { Controls } from './components/Controls/Controls';
-// import { GoogleGenAI } from "@google/genai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-// const apiKey = import.meta.env.VITE_GOOGLE_AI_API_KEY;
-
-// if (!apiKey) {
-//   throw new Error("API key is missing. Check your .env file and Vite config.");
-// }
-
-// const ai = new GoogleGenAI({apiKey: apiKey});
-// const gemini = ai.models.generateContent({
-//   model: "gemini-1.5-flash"
-// });
-// const chat = gemini.startChat({history: []});
+const ai = new GoogleGenerativeAI(import.meta.env.VITE_GOOGLE_AI_API_KEY);
+const gemini = ai.getGenerativeModel({ model: "gemini-1.5-flash" });
+const chat = gemini.startChat({history: []});
 
 function App() {
   const [messages, setMessages] = useState([]);
 
-  function handleSend(content){
-    setMessages((prevMessages) => [...prevMessages, {
-      content,
-      role: 'user'
-    }]);
+  function addMessage(message){
+    setMessages((prevMessages) => [...prevMessages, message]);
+
+  }
+
+  async function handleSend(content){
+    addMessage({content, role: "user"});
+    try {
+      const res = await chat.sendMessage(content);
+      addMessage({ content: res.response.text(), role: "assistant"});
+
+    } catch (error) {
+      addMessage({content: "Sorry, I couldn't process your request, Please try again." , role: "system"})
+    }
   }
 
   return (
